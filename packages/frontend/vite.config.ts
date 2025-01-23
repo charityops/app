a19bbc67eab2b4cd5638c8643d971b0980f06505
Dan Klco <klcodanr@users.noreply.github.com>
2025-01-22 23:06:12 -0500

    Create build.yml (#6)

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   plugins: [react() as any],
   build: {
     chunkSizeWarningLimit: 800,
