dc7e97ae7d43ae1ec395bfe7ff22db902000a1a4
Dan Klco <klcodanr@users.noreply.github.com>
2025-01-23 13:16:04 +0000

    Working on supporting cognito for auth

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 export function App() {
   return (
@@ -7,6 +8,11 @@ export function App() {
         <Navbar collapseOnSelect bg="dark-subtle" expand="md" className="px-3" data-bs-theme="dark">
           <Navbar.Brand className="fw-bold text-muted">CharityOps</Navbar.Brand>
           <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav>
+              <Nav.Link href="#/contacts">Contacts</Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
         </Navbar>
       </header>
       <main className="App container bg-dark text-light">
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,9 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from "react-router-dom";
+import { Amplify } from 'aws-amplify';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
-import {App} from './App.tsx'
+import {App} from './App.tsx';
+import {config} from './config.ts';
+
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolClientId: config.cognito.APP_CLIENT_ID,
+    }
+  },
+  API: {
+    REST: {
+      endpoint: config.apiGateway.URL,
+      region: config.apiGateway.REGION,
+    }
+  },
+});
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
